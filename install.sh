4ac388b2532865dd30575fceed8e34e2500466c0
Ilya Solovyev <mail@erdenian.ru>
2018-11-10 19:02:06 +0300

    install.sh enhancements

--- install.sh
+++ install.sh
@@ -4,35 +4,37 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+HOST=https://raw.githubusercontent.com/Erdenian/Ubuntu-VDS-installer/master
+
 apt update
 apt full-upgrade -y
 
-# russian locale
+echo Installing russian locale
 apt install -y language-pack-ru
 update-locale LANG=ru_RU.UTF-8
 
 apt install -y openjdk-8-jdk openjfx wget
 
-# git
+echo Installing git
 apt install -y software-properties-common
 add-apt-repository -y ppa:git-core/ppa
 apt update
 apt install -y git
 
-# jenkins
+echo Installing Jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt update
 apt install -y jenkins
-wget https://raw.githubusercontent.com/Erdenian/Ubuntu-VDS-installer/master/jenkins
+wget -O jenkins $HOST/jenkins
 mv jenkins /etc/default/jenkins
 /etc/init.d/jenkins restart
 
-# apache2
+echo Installing Apache
 apt install -y apache2
 a2enmod proxy
 a2enmod proxy_http
-wget https://raw.githubusercontent.com/Erdenian/Ubuntu-VDS-installer/master/jenkins.conf
+wget -O jenkins.conf $HOST/jenkins.conf
 mv jenkins.conf /etc/apache2/sites-available/jenkins.conf
 a2ensite jenkins
 apache2ctl restart
