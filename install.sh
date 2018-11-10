bfd93a33345ae2e01a3895a93147b2d3d0eb0bb6
Ilya Solovyev <mail@erdenian.ru>
2018-11-10 18:30:56 +0300

    Added Jenkins site for Apache

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ apt full-upgrade -y
 apt install -y language-pack-ru
 update-locale LANG=ru_RU.UTF-8
 
-apt install -y openjdk-8-jdk openjfx apache2 wget
+apt install -y openjdk-8-jdk openjfx wget
 
 # git
 apt install -y software-properties-common
@@ -26,4 +26,15 @@ apt update
 apt install -y jenkins
 wget https://raw.githubusercontent.com/Erdenian/Ubuntu-VDS-installer/master/jenkins
 mv jenkins /etc/default/jenkins
+
+# apache2
+apt install -y apache2
+a2enmod proxy
+a2enmod proxy_http
+
+wget https://raw.githubusercontent.com/Erdenian/Ubuntu-VDS-installer/master/jenkins.conf
+mv jenkins.conf /etc/apache2/sites-available/jenkins.conf
+
+a2ensite jenkins
+apache2ctl restart
 
