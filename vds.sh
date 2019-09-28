3288418b9c1dcebc994a92a403a2da9caa2a11fd
Ilya Solovyov <mail@erdenian.ru>
2019-09-28 18:22:44 +0300

    Install Gitlab Runner

--- vds.sh
+++ vds.sh
@@ -7,6 +7,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 HOST=https://raw.githubusercontent.com/Erdenian/Installers/master/vds
+HOSTNAME=vds.erdenian.ru
 ANDROID_SDK_TOOLS_LINK=https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 ANDROID_SDK_HOME=/opt/android
 ANDROID_SDK_ROOT=$ANDROID_SDK_HOME/sdk
@@ -26,12 +27,15 @@ function color_echo() {
     echo -e "\e[32m$1\e[0m"
 }
 
+color_echo 'Setting hostname...'
+echo $HOSTNAME > /etc/hostname
+
 color_echo 'Ugrading packages...'
 apt update
 apt full-upgrade -y
 
 color_echo 'Creating swap file...'
-fallocate -l 4G /swapfile
+fallocate -l 8G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
@@ -42,31 +46,30 @@ apt install -y language-pack-ru
 update-locale LANG=ru_RU.UTF-8
 
 color_echo 'Installing common packages...'
-apt install -y wget fail2ban
+apt install -y fail2ban wget curl unzip
 apt install -y software-properties-common # contains add-apt-repository
-apt install -y unzip # to unzip android sdk
-
-color_echo 'Adding repositories...'
-# jenkins
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -
-echo 'deb http://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/jenkins.list
-# git
-add-apt-repository -y ppa:git-core/ppa
-# openjdk
-add-apt-repository -y ppa:openjdk-r/ppa
 
 color_echo 'Installing OpenJDK...'
-apt install -y openjdk-8-jdk # Jenkins doesn't support Java 12, 8 will be set as default
-apt install -y openjdk-12-jdk
+add-apt-repository -y ppa:openjdk-r/ppa
+apt install -y openjdk-8-jdk # Jenkins doesn't support Java 13, 8 will be set as default
+apt install -y openjdk-13-jdk
 update-java-alternatives --set java-1.8.0-openjdk-amd64
 
 color_echo 'Installing Git...'
+add-apt-repository -y ppa:git-core/ppa
 apt install -y git
 
 color_echo 'Installing Jenkins...'
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add -
+echo 'deb http://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/jenkins.list
+apt update
 apt install -y jenkins
 download_from_host jenkins /etc/default/
 /etc/init.d/jenkins restart
+
+color_echo 'Installing Gitlab Runner...'
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash
+apt install gitlab-runner
 
 color_echo 'Installing Apache...'
 apt install -y apache2
@@ -79,15 +82,18 @@ color_echo 'Installing Android SDK...'
 wget -O android-sdk-tools.zip $ANDROID_SDK_TOOLS_LINK
 mkdir -p $ANDROID_SDK_ROOT
 unzip android-sdk-tools.zip -d $ANDROID_SDK_ROOT
-chmod -R 777 $ANDROID_SDK_ROOT
+chmod -R 777 $ANDROID_SDK_HOME
 rm android-sdk-tools.zip
-echo "ANDROID_SDK_HOME=$ANDROID_SDK_HOME" | sudo tee -a /etc/environment > /dev/null
-echo "ANDROID_SDK_ROOT=$ANDROID_SDK_ROOT" | sudo tee -a /etc/environment > /dev/null
+set +o pipefail
+yes | $ANDROID_SDK_ROOT/tools/bin/sdkmanager --licenses > /dev/null
+set -o pipefail
+echo "ANDROID_SDK_HOME=$ANDROID_SDK_HOME" >> /etc/environment
+echo "ANDROID_SDK_ROOT=$ANDROID_SDK_ROOT" >> /etc/environment
 
 color_echo 'Installing Gradle...'
 mkdir -p $GRADLE_USER_HOME
 chmod -R 777 $GRADLE_USER_HOME
-echo "GRADLE_USER_HOME=$GRADLE_USER_HOME" | sudo tee -a /etc/environment > /dev/null
+echo "GRADLE_USER_HOME=$GRADLE_USER_HOME" >> /etc/environment
 
 color_echo 'Post installation interaction...'
 
