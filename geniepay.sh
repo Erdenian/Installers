e2332959c5d65f4855e0a7a101e4dbd6e727ba15
Ilya Solovyov <mail@erdenian.ru>
2019-10-09 19:30:13 +0300

    Add script for GeniePay repositories

---
+++ geniepay.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -o errexit
+
+git config user.name "Ilya Solovyov"
+git config user.email ilya@geniepay.io
+git config commit.gpgsign true
+git config user.signingkey A538B710ECD5090E
